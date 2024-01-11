f5889aa439f1a431fdf88212697fbfe69a7c7483
Aashir Israr <aashirisrar968@gmail.com>
2024-01-11 19:05:24 +0500

    Add TextareaForm component and update dependencies

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import { MainNavigationMenu } from "@/components/navbar";
 import Hero from "@/components/hero";
 import {getSession} from "@/lib/session";
+import { TextareaForm } from "@/components/blog-form";
 
 export default async function Home() {  
   return (
@@ -10,6 +11,7 @@ export default async function Home() {
       <main className="flex-1">
         <div className="container relative">
           <Hero />
+          <TextareaForm />
         </div>
       </main>
     </div>
