a0cf596d5e4d3139d37d61daa8958632d509cd49
Aashir Israr <aashirisrar968@gmail.com>
2024-01-11 05:37:09 +0500

    adding the sign in functionality

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { MainNavigationMenu } from "@/components/navbar";
 import Hero from "@/components/hero";
+import {getSession} from "@/lib/session";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getSession();
+  console.log(user);
   return (
     <div className="relative flex min-h-screen flex-col bg-background">
       <MainNavigationMenu />
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from 'react'
+import { signIn } from 'next-auth/react'
 
 const LandingPage = () => {
   return (
-    <a href='/dashboard'>LandingPage</a>
+      <button onClick={() => signIn("google", { callbackUrl: 'http://localhost:3000/dashboard' })}>Sign in with Google</button>
   )
 }
 
