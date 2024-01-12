9f54eb6fa1ab4ca851d154421ed39b6e6bfd951d
Aashir Israr <aashirisrar968@gmail.com>
2024-01-13 02:53:56 +0500

    Update page layout in newpost and posts routes

--- app/(dashboard)/(routes)/newpost/page.tsx
+++ app/(dashboard)/(routes)/newpost/page.tsx
@@ -2,9 +2,10 @@ import { TextareaForm } from "@/components/blog-form";
 
 const NewPost = () => {
   return (
-    <div>
+    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+      <div className="text-4xl font-semibold my-6">Add New Post</div>
       <TextareaForm />
-    </div>
+    </section>
   );
 };
 
--- app/(dashboard)/(routes)/posts/page.tsx
+++ app/(dashboard)/(routes)/posts/page.tsx
@@ -2,9 +2,10 @@ import Posts from "@/components/posts";
 
 const PostsPage = () => {
   return (
-    <div>
+    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+      <div className="text-4xl font-semibold my-6">All Posts</div>
       <Posts />
-    </div>
+    </section>
   );
 };
 
